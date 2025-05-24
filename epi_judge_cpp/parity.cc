eb6e9b32fb9ff74b0edc83afad506183a10d9a26
cloud0233 <111362264+cloud0233@users.noreply.github.com>
2025-05-25 03:33:00 +0800

    pass parity

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,6 +1,13 @@
 #include "test_framework/generic_test.h"
 short Parity(unsigned long long x) {
-  // TODO - you fill in here.
+    x ^= x >> 32;
+    x ^= x >> 16;
+    x ^= x >> 8;
+    x ^= x >> 4;
+    x ^= x >> 2;
+    x ^= x >> 1;
+    x &= 1;
+    return x;
   return 0;
 }
 
